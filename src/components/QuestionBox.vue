0820bf4c00eb42861822cd47aa47047fd5ce5840
Thi Chieu <thi@bespokify.com>
2021-07-22 15:33:00 +0700

    first commit

---
+++ src/components/QuestionBox.vue
@@ -0,0 +1,33 @@
+<template>
+  <div>
+    <b-jumbotron>
+      <template #lead>
+        <span v-html="currentQuestion"></span>
+      </template>
+
+      <hr class="my-4" />
+
+      <p>List of answer</p>
+
+      <b-button variant="primary" href="#">Submit</b-button>
+      <b-button variant="success" href="#" @click="onClick"
+        >Next question</b-button
+      >
+    </b-jumbotron>
+  </div>
+</template>
+<script>
+export default {
+  props: {
+    currentQuestion: String,
+  },
+  methods: {
+    onClick() {
+      this.$emit("nextQuestion");
+    },
+  },
+};
+</script>
+
+
+
